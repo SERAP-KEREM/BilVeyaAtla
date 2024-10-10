ec21ba4c7bbe9896bfbe79240be914f60f055466
Serap Kerem <66235306+SERAP-KEREM@users.noreply.github.com>
2024-10-10 23:39:40 +0300

    FindRoom page added.

--- Assets/Scripts/MultiplayerScripts/Launcher.cs
+++ Assets/Scripts/MultiplayerScripts/Launcher.cs
@@ -2,9 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Realtime;
 public class Launcher : MonoBehaviourPunCallbacks
 {
-    
+    public static Launcher Instance;
+
+    [SerializeField] TMP_InputField roomNameInputField;
+
+    [SerializeField] TextMeshProUGUI roomNameText;
+
+    [SerializeField] TextMeshProUGUI errorText;
+
+    [SerializeField] Transform roomListContent;
+
+    [SerializeField] GameObject roomListItemPrefab;
+
+    private void Awake()
+    {
+        Instance = this;    
+    }
+
     void Start()
     {
         Debug.Log("Connecting to Master...");
@@ -14,7 +33,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected to Master");
-        OnJoinedLobby();
+        PhotonNetwork.JoinLobby();
 
     }
 
@@ -23,6 +42,58 @@ public class Launcher : MonoBehaviourPunCallbacks
 
         MenuManager.instance.OpenMenu("TitleMenu");
         Debug.Log("Joined Lobby");
+    }
+
+    public void CreateRoom()
+    {
+        if(string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.instance.OpenMenu("LoadingMenu");
+
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.instance.OpenMenu("RoomMenu");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name; 
+    }
+    public override void OnCreateRoomFailed(short returnCode,string errormessage)
+    {
+        errorText.text = "Room Generation Unsuccesfull" + errormessage;
+        MenuManager.instance.OpenMenu("ErrorMenu");
+
+    }
+
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.instance.OpenMenu("LoadingMenu");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.instance.OpenMenu("LoadingMenu");
+    }
+    public override void OnLeftRoom()
+    {
+        MenuManager.instance.OpenMenu("TitleMenu");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
     }
 
 
---
+++ Assets/Scripts/MultiplayerScripts/RoomListItem.cs
@@ -0,0 +1,27 @@
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using TMPro;
+
+public class RoomListItem : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI roomNameText;
+
+    RoomInfo info;
+
+   
+
+    public void SetUp(RoomInfo _info)
+    {
+        info = _info;
+        roomNameText.text = info.Name;
+    }
+
+    public void OnClick()
+    {
+        Launcher.Instance.JoinRoom(info);
+    }
+        
+}
